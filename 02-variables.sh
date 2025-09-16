02d62c140fb5760f0f245d1719ece123cde326e6
harish reddy <harishreddy1087@gmail.com>
2025-09-16 14:44:41 +0530

    worked on varible format

---
+++ 02-variables.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# writing the sheel for learn
+
+# here iam variablizing the content this is the best practice
+
+echo "deep dive to learn the sheel from BASIC to ADVANCED in variable"
+
+PERSON1=giri
+PERSON2=nikki
+# here iam applying the varible
+
+echo "person1=giri and person2=nikki"
+
+echo "$person1: hi how are you"
+echo "$person2: hi $person1 im good how are u"
+echo "$person1: im good $person2"
+echo "$person2: learining shell $person1"
+
+# by doing the above format we can skip the errors and no accidental changes
