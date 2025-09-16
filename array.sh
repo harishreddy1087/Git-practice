3407ddc0ba551c8189b8485261bf651dbc095941
harish reddy <harishreddy1087@gmail.com>
2025-09-16 16:04:22 +0530

    checking the result

--- array.sh
+++ array.sh
@@ -1,9 +1,9 @@
 #!bin/bash
 
 # here im writing to explain the arrys functions
-#FRIUTS=("APPLE" "MANGO" "KIWI")
-FRIUT="MANGO"
+FRIUTS=("APPLE" "MANGO" "KIWI")
+#FRIUT="MANGO"
 
-echo "$FRIUT"
+#echo "$FRIUT"
 
-#echo "first friut name is: $FRIUTS"
+echo "first friut name is: $FRIUTS"
