28853484ca450fc7aaf6bfe0a58b5a13d768ddb9
harish reddy <harishreddy1087@gmail.com>
2025-09-16 14:27:36 +0530

    added the conversion b/w friends

---
+++ 01-conversion.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# writing the sheel for learn
+
+echo "deep dive to learn the sheel from BASIC to ADVANCED"
+
+echo "hari: hi how are you"
+echo "reddy: hi hari im good how are u"
+echo "hari: im good"
+echo "reddy: learining shell hari"
