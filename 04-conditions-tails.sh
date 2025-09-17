ebc2c4948ac4b720209cbdb056a98fca6d50676f
harish reddy <harishreddy1087@gmail.com>
2025-09-17 19:34:04 +0530

    checking the result

--- 04-conditions-tails.sh
+++ 04-conditions-tails.sh
@@ -9,7 +9,7 @@ USERID=$(id -u)
 #checking the permission
 # -gt, -lt, -ne, -ge, -le, -eq
 
-if [$USERID -ne 0 ]
+if [ $USERID -ne 0 ]
     then
     echo "please run the script with root user"
     exit 1
@@ -19,11 +19,11 @@ fi
 
 dnf install git -y
 # $?== duty of this command is to check the previous command is executed or not...
-if [$? -ne 0 ]
+if [ $? -ne 0 ]
     then
      echo "git is not installed going to install"
      dnf install git -y # installing the git
-    if [$? -ne 0 ]
+    if [ $? -ne 0 ]
     then
      echo "git installation is success"
      exit 1
@@ -31,12 +31,12 @@ if [$? -ne 0 ]
      echo "git is already installed...."
 fi
 
-git status --version
+git --version
 
-if [$? -ne 0 ]
+if [ $? -ne 0 ]
     then
      echo "git is not instlled"
 else
-    echo "check the installation process"
+    echo "git version is $(git --version)"
 
 
