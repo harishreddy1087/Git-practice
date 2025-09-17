987efbef842b6234ee7f952cdd88b602379fbe8d
harish reddy <harishreddy1087@gmail.com>
2025-09-17 14:37:52 +0530

    checking the result

--- special-variables.sh
+++ special-variables.sh
@@ -3,4 +3,8 @@
 echo "below practice is most use full for the interview purpose"
 
 #want to pass the all variables to the script
+# harish reddy 120
+#below practice is most use full for the interview purpose
+#all variables passed to the script: harish reddy 120 this is final output when u execute
+
 echo "all variables passed to the script: $@"
