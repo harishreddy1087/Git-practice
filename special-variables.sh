470a7da7cb29cf4723e8415aabf75ad5628e0ff6
harish reddy <harishreddy1087@gmail.com>
2025-09-17 14:41:52 +0530

    checking the result

--- special-variables.sh
+++ special-variables.sh
@@ -4,7 +4,12 @@ echo "below practice is most use full for the interview purpose"
 
 #want to pass the all variables to the script
 # harish reddy 120
-#below practice is most use full for the interview purpose
+#while executing we have to pass the inputs
 #all variables passed to the script: harish reddy 120 this is final output when u execute
 
 echo "all variables passed to the script: $@"
+
+
+
+# want to know the how may number of variables are passed to the script during executions
+echo "how many of variables are passed: $#"
