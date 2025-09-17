db770ad2cab8d7c033cb0468f39cc3bb472c3d0b
harish reddy <harishreddy1087@gmail.com>
2025-09-17 16:54:15 +0530

    checking the result

--- 04-conditions-tails.sh
+++ 04-conditions-tails.sh
@@ -1,7 +1,31 @@
 #/bin/bash
 
-# here i want to install the git
+# here i checking the user id
 
 USERID=$(id -u)
 
-echo " user id is $USERID"
+#echo " user id is $USERID"
+
+#checking the permission
+
+if [$USERID -ne 0 ]
+    then
+    echo "please run the script with root user"
+    exit 1
+fi
+
+#installing the git
+
+dnf install git -y
+
+if [$? -ne 0]
+    then
+    echo "git is not installed going to install"
+    dnf install git -y # installing the git
+    if [$? -ne 0 ]
+    then
+    echo "git installation is success"
+    exit 1
+    else
+    
+
