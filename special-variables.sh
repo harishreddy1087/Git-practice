0a55eb2b31a07324552ec3ce25d9e66ecfa9bd8a
harish reddy <harishreddy1087@gmail.com>
2025-09-17 14:29:19 +0530

    checking the result

--- special-variables.sh
+++ special-variables.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-echo "below practice is most sue full for thr interview purpose"
+echo "below practice is most use full for the interview purpose"
 
 #want to pass the all variables to the script
 echo "all variables passed to the script": $@"
